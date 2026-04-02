2bf5edf9448b738a88d744aaae895c8bc7029930
BeastBear <phurich_10@hotmail.com>
2026-04-02 21:40:04 +0700

    fixed save button

--- src/pages/AddOption.jsx
+++ src/pages/AddOption.jsx
@@ -474,8 +474,6 @@ function AddOption() {
                             })
                           }
                           placeholder="Enter amount"
-                          min="0"
-                          step="0.01"
                           className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-orange/20 focus:border-primary-orange"
                         />
                       </div>
@@ -508,8 +506,6 @@ function AddOption() {
                             })
                           }
                           placeholder="Enter amount"
-                          min="0"
-                          step="0.01"
                           className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-orange/20 focus:border-primary-orange"
                         />
                       </div>
